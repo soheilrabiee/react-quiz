4a6d2e2b215f6cfb693600e9da9f70cfd507991e
Soheil <iamsoheil.r@gmail.com>
2025-09-09 21:47:19 +0330

    Add json server and fetch data locally

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { useEffect } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
 export default function App() {
+    useEffect(function () {
+        async function getData() {
+            try {
+                const res = await fetch("http://localhost:9000/questions");
+                const data = await res.json();
+                if (!res.ok) {
+                    throw new Error("Couldn't fetch the data");
+                }
+                console.log(data);
+            } catch (err) {
+                console.error(err.message);
+            }
+        }
+
+        getData();
+    }, []);
+
     return (
         <div className="app">
             <Header />
