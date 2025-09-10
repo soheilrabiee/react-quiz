a8e8de77a85c59e760de523b69a7e924e20ae5e7
Soheil <iamsoheil.r@gmail.com>
2025-09-10 19:50:37 +0330

    Complete the timer

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,8 @@ import FinishScreen from "./FinishScreen";
 import Footer from "./Footer";
 import Timer from "./Timer";
 
+const SECS_PER_QUESTION = 30;
+
 const initialState = {
     questions: [],
 
@@ -21,7 +23,7 @@ const initialState = {
     answer: null,
     points: 0,
     highscore: 0,
-    secondsRemaining: 10,
+    secondsRemaining: null,
 };
 
 function reducer(state, action) {
@@ -38,7 +40,11 @@ function reducer(state, action) {
                 status: "error",
             };
         case "start":
-            return { ...state, status: "active" };
+            return {
+                ...state,
+                status: "active",
+                secondsRemaining: state.questions.length * SECS_PER_QUESTION,
+            };
         case "newAnswer":
             const question = state.questions.at(state.index);
 
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
 
 function Timer({ dispatch, secondsRemaining }) {
+    const mins = Math.floor(secondsRemaining / 60);
+    const seconds = secondsRemaining % 60;
+
     useEffect(
         function () {
             const id = setInterval(function () {
@@ -12,7 +15,13 @@ function Timer({ dispatch, secondsRemaining }) {
         [dispatch]
     );
 
-    return <div className="timer">{secondsRemaining}</div>;
+    return (
+        <div className="timer">
+            {mins < 10 && "0"}
+            {mins}:{seconds < 10 && "0"}
+            {seconds}
+        </div>
+    );
 }
 
 export default Timer;
