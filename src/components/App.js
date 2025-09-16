1375ae39b80a2dce857c48af3e9fc6366c9540eb
Soheil <iamsoheil.r@gmail.com>
2025-09-16 18:53:44 +0330

    Refactor the app.js file

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 import Loader from "./Loader";
@@ -10,56 +9,29 @@ import Progress from "./Progress";
 import FinishScreen from "./FinishScreen";
 import Footer from "./Footer";
 import Timer from "./Timer";
+import { useQuiz } from "../context/QuizContext";
 
 export default function App() {
+    const { status } = useQuiz();
+
     return (
         <div className="app">
             <Header />
             <Main>
                 {status === "loading" && <Loader />}
                 {status === "error" && <Error />}
-                {status === "ready" && (
-                    <StartScreen
-                        numQuestions={numQuestions}
-                        dispatch={dispatch}
-                    />
-                )}
+                {status === "ready" && <StartScreen />}
                 {status === "active" && (
                     <>
-                        <Progress
-                            index={index}
-                            numQuestions={numQuestions}
-                            points={points}
-                            maxPossiblePoints={maxPossiblePoints}
-                            answer={answer}
-                        />
-                        <Question
-                            question={questions[index]}
-                            dispatch={dispatch}
-                            answer={answer}
-                        />
+                        <Progress />
+                        <Question />
                         <Footer>
-                            <Timer
-                                dispatch={dispatch}
-                                secondsRemaining={secondsRemaining}
-                            />
-                            <NextButton
-                                dispatch={dispatch}
-                                answer={answer}
-                                numQuestions={numQuestions}
-                                index={index}
-                            />
+                            <Timer />
+                            <NextButton />
                         </Footer>
                     </>
                 )}
-                {status === "finished" && (
-                    <FinishScreen
-                        points={points}
-                        maxPossiblePoints={maxPossiblePoints}
-                        highscore={highscore}
-                        dispatch={dispatch}
-                    />
-                )}
+                {status === "finished" && <FinishScreen />}
             </Main>
         </div>
     );
